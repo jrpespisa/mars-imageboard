ed8268e6cb19d5f3d6e59c122173c8d6c82f106c
jrpespisa <jrpespisa@verizon.net>
2016-01-23 19:33:32 -0500

    made existing feature tests more DRY

--- spec/features/user_can_comment_on_image_spec.rb
+++ spec/features/user_can_comment_on_image_spec.rb
@@ -1,33 +1,19 @@
 require 'rails_helper'
 
 feature "user can add comment to a specific image" do
-  let(:rover1) { FactoryGirl.create(:rover, name: "Rover_1") }
-  let(:rover2) { FactoryGirl.create(:rover, name: "Rover_2") }
-  let(:rover3) { FactoryGirl.create(:rover, name: "Rover_3") }
-
-  let(:image1) { FactoryGirl.create(:image, name: "Image_1", rover_id: rover1.id) }
-  let(:image2) { FactoryGirl.create(:image, name: "Image_2", rover_id: rover2.id) }
-  let(:image3) { FactoryGirl.create(:image, name: "Image_3", rover_id: rover3.id) }
-
-  let(:user) { FactoryGirl.create(:user)}
 
   before :each do
-    rover1
-    rover2
-    rover3
-    image1
-    image2
-    image3
+    comment
   end
 
   scenario "user successfully adds comment" do
-    comment
 
     expect(page).to have_content "Here is a test comment"
     expect(page).to have_content "Add Comment"
   end
 
   scenario "user cannot add comment if they are not logged in" do
+    click_on "Sign Out"
     visit root_path
     click_on "Rover_1"
     page.first(".link").click
@@ -37,14 +23,14 @@ feature "user can add comment to a specific image" do
 
   end
   scenario "user cannot visit the url path for new comment without signing in" do
-    visit new_image_comment_path(image1)
+    click_on "Sign Out"
+    visit new_image_comment_path(@image1)
 
     expect(page).to have_content("You need to sign in or sign up before continuing.")
     expect(page).to have_content "Log In"
   end
 
   scenario "user can edit their own comment" do
-    comment
     click_on "Edit Comment"
     fill_in "Comment", with: "This is a different comment"
     click_button "Update"
@@ -54,7 +40,6 @@ feature "user can add comment to a specific image" do
   end
 
   scenario "user can delete their own comment" do
-    comment
     click_on "Delete Comment"
 
     expect(page).to_not have_content "Here is a test comment"
@@ -63,9 +48,10 @@ feature "user can add comment to a specific image" do
 end
 
 def comment
+  test_seeder
   visit new_user_session_path
-  fill_in "Email", with: user.email
-  fill_in "Password", with: user.password
+  fill_in "Email", with: @user.email
+  fill_in "Password", with: @user.password
   click_button "Sign In"
   click_on "Rover_1"
   page.first(".link").click
---
+++ spec/features/user_can_vote_on_image_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+feature "user can upvote or downvote images" do
+
+  before :each do
+    test_seeder
+  end
+
+
+end
